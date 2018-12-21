054fb56a58fdfbdb978d7e9fb7d1d9f3d9b64859
pabloborrajo1988 <45261360+pabloborrajo1988@users.noreply.github.com>
2018-12-21 10:53:37 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,3 @@
+var dias="Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo";
+var lugares="Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti";
+var matriz=[dias.split(","),lugares.split(".")];
